cd4806ad05cfdfe41dc5679cd365068ee17a7f75
Premk192003 <premk19203@gmail.com>
2024-10-06 22:28:00 -0400

    BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,44 @@
+public class BankAccount {
+    private String accountHolderName;
+    private String accountNumber;
+    private double balance;
+
+    // Constructor
+    public BankAccount(String accountHolderName, String accountNumber) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = 0.0; // Default balance is 0
+    }
+
+    // Get the account balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Deposit money into the account
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    // Withdraw money from the account
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount");
+        }
+    }
+
+    // Display account information
+    public void displayAccountInfo() {
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: $" + balance);
+    }
+}
